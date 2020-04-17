bdddc49cd29ee29364b57556d859e5fe6e4ea5ef
Nicolas Soares <nicolas.soaresh.98@gmail.com>
2020-04-17 10:55:02 -0300

    Projeto criado

---
+++ src/main/java/com/ares/springionic/SpringIonicApplication.java
@@ -0,0 +1,13 @@
+package com.ares.springionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIonicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ares/springionic/SpringIonicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ares.springionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIonicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
