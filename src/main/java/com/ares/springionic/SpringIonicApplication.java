e3efc9736b4c11abff69e3e7dc9fd131482ff5a8
Nicolas Soares <nicolas.soaresh.98@gmail.com>
2020-04-23 17:42:39 -0300

    Produto implementado e associação ManyToMany configurada

--- src/main/java/com/ares/springionic/SpringIonicApplication.java
+++ src/main/java/com/ares/springionic/SpringIonicApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ares.springionic.repositories.CategoriaRepository;
+import com.ares.springionic.repositories.ProdutoRepository;
 import com.ares.springionic.resources.domain.Categoria;
+import com.ares.springionic.resources.domain.Produto;
 
 @SpringBootApplication
 public class SpringIonicApplication implements CommandLineRunner{
 
 	@Autowired
-	private CategoriaRepository repository;
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIonicApplication.class, args);
@@ -26,7 +31,19 @@ public class SpringIonicApplication implements CommandLineRunner{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
-		repository.saveAll(Arrays.asList(cat1, cat2));
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().add(p2);
+		
+		p1.getCategorias().add(cat1);
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().add(cat1);
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 
