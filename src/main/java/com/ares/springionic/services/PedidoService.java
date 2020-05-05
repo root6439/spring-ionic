c34a32a0f5b7f20c3b5167e41a2bfeaab0e269aa
root6439 <nicolas.soaresh.98@gmail.com>
2020-05-05 16:32:02 -0300

    Endpoint para pedidos feito

---
+++ src/main/java/com/ares/springionic/services/PedidoService.java
@@ -0,0 +1,24 @@
+package com.ares.springionic.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ares.springionic.repositories.PedidoRepository;
+import com.ares.springionic.resources.domain.Pedido;
+import com.ares.springionic.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repository;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> pedido = repository.findById(id);
+		return pedido.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado. Id: " + id + ", tipo: " + Pedido.class.getName()));
+	}
+	
+}
